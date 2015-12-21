de4d0421be921cb442dc67d7afc0c045c80d0b18
Dale Hamel <dale.hamel@shopify.com>
2015-12-21 00:21:46 -0500

    Support audit mode

--- lib/chef-provisioner/bootstrap.rb
+++ lib/chef-provisioner/bootstrap.rb
@@ -9,14 +9,14 @@ module ChefProvisioner
     extend self
     BOOTSTRAP_TEMPLATE = File.read(File.expand_path('../templates/bootstrap.erb', __FILE__)).freeze
 
-    def generate(node_name: '', chef_version: '12.4.1', environment: nil, server: '', first_boot: {}, reinstall: false)
+    def generate(node_name: '', chef_version: '12.4.1', environment: nil, server: '', first_boot: {}, reinstall: false, audit: false)
       node_name = node_name.strip
       server = ChefAPI.endpoint if server.empty?
       run_list = first_boot[:run_list] if first_boot[:run_list] # FIXME - symbolize keys instead of the dup here
       run_list = first_boot['run_list'] if first_boot['run_list']
       client_pem = ChefProvisioner::Chef.init_server(node_name, run_list: (run_list || []))
       first_boot.merge!( fqdn: node_name, chef_client: {config: {chef_server_url: server}} )
-      render(node_name: node_name, client_pem: client_pem, chef_version: chef_version, environment: environment, server: server, first_boot: first_boot, reinstall: reinstall)
+      render(node_name: node_name, client_pem: client_pem, chef_version: chef_version, environment: environment, server: server, first_boot: first_boot, reinstall: reinstall, audit: audit)
     end
 
     private
