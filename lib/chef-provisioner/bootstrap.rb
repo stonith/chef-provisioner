c43d3dd6bdba28dbb3ee6c797100b3b73cdc2601
Dale Hamel <dale.hamel@shopify.com>
2016-04-20 20:01:38 -0400

    Support ruby profiling

--- lib/chef-provisioner/bootstrap.rb
+++ lib/chef-provisioner/bootstrap.rb
@@ -9,14 +9,14 @@ module ChefProvisioner
     extend self
     BOOTSTRAP_TEMPLATE = File.read(File.expand_path('../templates/bootstrap.erb', __FILE__)).freeze
 
-    def generate(node_name: '', chef_version: '12.4.1', environment: nil, server: '', first_boot: {}, reinstall: false, audit: false, force: false, retries: 1, chef_cmd: nil)
+    def generate(node_name: '', chef_version: '12.4.1', environment: nil, server: '', first_boot: {}, reinstall: false, audit: false, force: false, retries: 1, profile: false, chef_cmd: nil)
       node_name = node_name.strip
       server = ChefAPI.endpoint if server.empty?
       run_list = first_boot[:run_list] if first_boot[:run_list] # FIXME - symbolize keys instead of the dup here
       run_list = first_boot['run_list'] if first_boot['run_list']
       first_boot.merge!( fqdn: node_name, chef_client: {config: {chef_server_url: server}} )
       client_pem = get_client_key(node_name, run_list, force, retries)
-      render(node_name: node_name, client_pem: client_pem, chef_version: chef_version, environment: environment, server: server, first_boot: first_boot, reinstall: reinstall, audit: audit)
+      render(node_name: node_name, client_pem: client_pem, chef_version: chef_version, environment: environment, server: server, first_boot: first_boot, reinstall: reinstall, audit: audit, profile: profile)
     end
 
     private
